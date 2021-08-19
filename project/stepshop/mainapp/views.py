49ea31f577c0e18a9ee6fc1b104992f899f06c4e
Vafailis <zhantore.ks@gmail.com>
2021-08-19 11:41:26 +0600

    lesson-3

--- project/stepshop/mainapp/views.py
+++ project/stepshop/mainapp/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 
 def products(req):
-    return render(req, 'mainapp\products.html')
+    title = 'продукты | каталог'
+
+    links_manu = [
+        {'data_filter': '*', 'name': 'All Products'},
+        {'data_filter': '.new', 'name': 'Newest'},
+        {'data_filter': '.low', 'name': 'Low Price'},
+        {'data_filter': '.high', 'name': 'Hight Price'},
+    ]
+
+    context = {
+        'title':title,
+        'links_menu': links_manu,
+    }
+
+    return render(request=req,template_name='mainapp\products.html', context=context)

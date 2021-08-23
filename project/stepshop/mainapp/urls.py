6a55ef32bcbe6ca8c4cb1ef2cafb75f8c9ed9052
Vafailis <zhantore.ks@gmail.com>
2021-08-23 09:47:33 +0600

    lesson_5

--- project/stepshop/mainapp/urls.py
+++ project/stepshop/mainapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import products
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = 'products'
@@ -7,3 +9,7 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='index'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
